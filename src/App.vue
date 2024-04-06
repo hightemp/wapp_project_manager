291585eb4bac6eb92e1aebb88ff3c869322341c7
Панов Антон <hightemp.unknown@gmail.com>
2024-04-06 23:55:04 +0300

    update

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,9 @@ import { IProjects } from "./types/Project"
 import { ISettings } from "./types/Settings"
 import { convertMarkdownToHTML } from './lib/markdown_converter';
 
+import { Splitpanes, Pane } from 'splitpanes'
+import 'splitpanes/dist/splitpanes.css'
+
 var settings = ref<ISettings>(<any>{});
 var projects = ref<IProjects>([]);
 var selected_project_index = ref<number>(-1)
@@ -27,7 +30,7 @@ function selectProject(index: number) {
 // добавьте после объявления переменных
 const selected_project_class = computed(() => {
   return (index:number) => {
-    return index === selected_project_index.value ? 'active' : '';
+    return index === selected_project_index.value;
   };
 });
 
@@ -40,22 +43,91 @@ const selected_project_description = computed(() => {
 </script>
 
 <template>
+  <v-app>
+    
+    <v-app-bar app >    
+      
+    </v-app-bar>
+    
+    <v-main>
+      <splitpanes class="default-theme">
+        <pane width="20" min-size="20" max-size="40">
+          <div class="left-pane">
+            <v-toolbar
+              dense
+              floating
+            >
+              <v-text-field
+                hide-details
+                single-line
+              ></v-text-field>
+
+              <v-btn icon>
+                <v-icon>mdi-dots-vertical</v-icon>
+              </v-btn>
+            </v-toolbar>
+            <div style="height:100%;overflow-y: scroll;">
+              <v-list lines="two">
+                <v-list-item
+                  v-for="(project, index) in projects"
+                  :key="index"
+                  :subtitle="project.path"
+                  :title="project.title"
+                  :active="selected_project_class(index)"
+                  @click="selectProject(index)"
+                >
+                  <template v-slot:prepend>
+                    <v-avatar color="grey-lighten-1">
+                      <v-icon color="white">mdi-folder</v-icon>
+                    </v-avatar>
+                  </template>
+
+                  <!--template v-slot:append>
+                    <v-btn
+                      color="grey-lighten-1"
+                      icon="mdi-information"
+                      variant="text"
+                    ></v-btn>
+                  </template-->
+                </v-list-item>
+              </v-list>
+            </div>
+          </div>
+        </pane>
+        <pane>
+          <iframe
+            :srcdoc="selected_project_description"
+            frameborder="0"
+            width="100%"
+            height="100%"
+          ></iframe>
+        </pane>
+      </splitpanes>
+    </v-main>
+    
+  </v-app>
+          <!--
   <div class="main">
     <div class="left-list">
-      <div class="list-group">
-        <template v-for="(project, index) in projects" v-bind:key="index">
-          <button 
-            type="button" 
-            :class="'list-group-item list-group-item-action ' + selected_project_class(index)"
-            @click="selectProject(index)"
-          >
-            <div class="ms-2 me-auto">
-              <div class="fw-bold">{{ project.title }}</div>
-              <small>{{ project.path }}</small>
-              {{ project.short_description }}
-            </div>
-          </button>
-        </template>
+      <div class="left-list-bar">
+      </div>
+      <div class="left-list_content-scroll">
+        <div class="list-group">
+
+          <template v-for="(project, index) in projects" v-bind:key="index">
+            <button 
+              type="button" 
+              :class="'list-group-item list-group-item-action ' + selected_project_class(index)"
+              @click="selectProject(index)"
+            >
+              <div class="ms-2 me-auto">
+                <div class="fw-bold">{{ project.title }}</div>
+                <small>{{ project.path }}</small>
+                {{ project.short_description }}
+              </div>
+            </button>
+          </template>
+        </div>
       </div>
     </div>
     <div class="right-panel">
@@ -63,13 +135,5 @@ const selected_project_description = computed(() => {
       </div>
     </div>
   </div>
+          -->
 </template>
-
-
-function convertMarkdownToHTML(markdown: any) {
-  throw new Error("Function not implemented.")
-}
-
-function convertMarkdownToHTML(markdown: any) {
-  throw new Error("Function not implemented.");
-}
